91424b4b62455aabe21ae4ea1813b7eaa1b275dc
Kazeshyn <idriss.merouane@gmail.com>
2024-01-11 13:43:30 +0100

    :art
    footer mis à jour

--- compte.php
+++ compte.php
@@ -37,90 +37,90 @@ if (isset($_SESSION["login"])) {
 </head>
 
 <body>
- 
-<header>
-            <!-- Header téléphone/tablettes -->
-            <nav class="phonetabheader">
-                <a href="./accueil.php" class="logoheader"><img class="logoheader" src="./img/logoUniversite2.png" alt="Retourner à l'accueil (page actuelle)"></a>
-                <div class="headergroupphone">
-                    <a href="./pasCree.html" class="darkm">☀️</a>
-                    <img src="./img/burger_menu.png" alt="" id="button">
-                </div>
-            </nav>
-            <!-- Modal du burger menu -->
-            <div class="menu menuoff" id="menu">
-                <p class="burgerprewrapper">Communication</p>
-                <div class="burgerwrapper">
-                    <a href="https://www.partage.univ-eiffel.fr/mail" class="navlink">Mail</a>
-                    <a href="./forum.php" class="navlink">Forum</a>
-                    <a href="./actu.php" class="navlink">Actualité</a>
-                </div>
-                <a href="" class="burgerprewrapper">Restauration</a>
-                <p class="burgerprewrapper">Ressources</p>
-                <div class="burgerwrapper">
-                    <a href="https://bu.univ-gustave-eiffel.fr/collections/ressources-electroniques/de-a-a-z/" class="navlink">Bibliothèque de ressources</a>
-                    <a href="https://etudiant.u-pem.fr/ent-services.php" class="navlink">Mes fichiers</a>
-                    <a href="./pasCree.html" class="navlink">Tutoriels</a>
-                    <a href="./reservation.php" class="navlink">Réservations</a>
-                </div>
-                <p class="burgerprewrapper">Cours</p>
-                <div class="burgerwrapper">
-                    <a href="https://elearning.univ-eiffel.fr/my/" class="navlink">Cours</a>
-                    <a href="https://edt.univ-eiffel.fr/direct/index.jsp?login=visuedt&password=visuedt" class="navlink">Emploi du temps</a>
-                    <a href="./note.php" class="navlink">Notes</a>
-                    <a href="https://elearning.univ-eiffel.fr/calendar/view.php?view=upcoming" class="navlink">Agenda</a>
-                </div>
-                <a href="./compte.php" class="burgerprewrapper">Mon compte</a>
+
+    <header>
+        <!-- Header téléphone/tablettes -->
+        <nav class="phonetabheader">
+            <a href="./accueil.php" class="logoheader"><img class="logoheader" src="./img/logoUniversite2.png" alt="Retourner à l'accueil (page actuelle)"></a>
+            <div class="headergroupphone">
+                <a href="./pasCree.html" class="darkm">☀️</a>
+                <img src="./img/burger_menu.png" alt="" id="button">
             </div>
-            <!-- Header ordinateur -->
-            <nav class="desktopheader">
-                <div class="conteneur-nav">
-                    <label for="mobile">Afficher / Cacher le menu</label>
-                    <input type="checkbox" id="mobile" role="button">
-                    <ul>
-                        <li class="deroulant align"><a href="#" class="linkcolor navlink">Communication &ensp;</a>
-                            <ul class="sous">
-                                <li><a href="https://www.partage.univ-eiffel.fr/mail">Mail</a></li>
-                                <li><a href="./forum.php">Forum</a></li>
-                                <li><a href="./actu.php">Actualité</a></li>
-                            </ul>
-                        </li>
-                        <li class="align"><a href="./restauration.php" class="linkcolor navlink">Restauration</a></li>
-                        <li class="deroulant align"><a href="#" class="linkcolor navlink">Ressources &ensp;</a>
-                            <ul class="sous">
-                                <li><a href="https://bu.univ-gustave-eiffel.fr/collections/ressources-electroniques/de-a-a-z/">Bibliothèque de ressources</a></li>
-                                <li><a href="./pasCree.html">Tutoriels</a></li>
-                                <li><a href="https://etudiant.u-pem.fr/ent-services.php">Mes fichiers</a></li>
-                            </ul>
-                        </li>
-    
-                        <li class="align"><a href="./accueil.php"><img src="./img/logoUniversite2.png" alt=""></a></li>
-    
-                        <li class="align"><a href="./reservation.php" class="linkcolor navlink">Reservation</a></li>
-                        <li class="deroulant align"><a href="#" class="linkcolor navlink">Cours &ensp;</a>
-                            <ul class="sous">
-                                <li><a href="https://elearning.univ-eiffel.fr/my/">Cours</a></li>
-                                <li><a href="https://edt.univ-eiffel.fr/direct/index.jsp?login=visuedt&password=visuedt">Emploi du temps</a></li>
-                                <li><a href="./note.php">Notes</a></li>
-                                <li><a href="https://elearning.univ-eiffel.fr/calendar/view.php?view=upcoming">Agenda</a></li>
-                            </ul>
-                        </li>
-                        <li class="align"><a href="./compte.php" class="linkcolor navlink">Mon compte</a></li>
-                    </ul>
-                </div>
-            </nav>
-        
+        </nav>
+        <!-- Modal du burger menu -->
+        <div class="menu menuoff" id="menu">
+            <p class="burgerprewrapper">Communication</p>
+            <div class="burgerwrapper">
+                <a href="https://www.partage.univ-eiffel.fr/mail" class="navlink">Mail</a>
+                <a href="./forum.php" class="navlink">Forum</a>
+                <a href="./actu.php" class="navlink">Actualité</a>
+            </div>
+            <a href="" class="burgerprewrapper">Restauration</a>
+            <p class="burgerprewrapper">Ressources</p>
+            <div class="burgerwrapper">
+                <a href="https://bu.univ-gustave-eiffel.fr/collections/ressources-electroniques/de-a-a-z/" class="navlink">Bibliothèque de ressources</a>
+                <a href="https://etudiant.u-pem.fr/ent-services.php" class="navlink">Mes fichiers</a>
+                <a href="./pasCree.html" class="navlink">Tutoriels</a>
+                <a href="./reservation.php" class="navlink">Réservations</a>
+            </div>
+            <p class="burgerprewrapper">Cours</p>
+            <div class="burgerwrapper">
+                <a href="https://elearning.univ-eiffel.fr/my/" class="navlink">Cours</a>
+                <a href="https://edt.univ-eiffel.fr/direct/index.jsp?login=visuedt&password=visuedt" class="navlink">Emploi du temps</a>
+                <a href="./note.php" class="navlink">Notes</a>
+                <a href="https://elearning.univ-eiffel.fr/calendar/view.php?view=upcoming" class="navlink">Agenda</a>
+            </div>
+            <a href="./compte.php" class="burgerprewrapper">Mon compte</a>
+        </div>
+        <!-- Header ordinateur -->
+        <nav class="desktopheader">
+            <div class="conteneur-nav">
+                <label for="mobile">Afficher / Cacher le menu</label>
+                <input type="checkbox" id="mobile" role="button">
+                <ul>
+                    <li class="deroulant align"><a href="#" class="linkcolor navlink">Communication &ensp;</a>
+                        <ul class="sous">
+                            <li><a href="https://www.partage.univ-eiffel.fr/mail">Mail</a></li>
+                            <li><a href="./forum.php">Forum</a></li>
+                            <li><a href="./actu.php">Actualité</a></li>
+                        </ul>
+                    </li>
+                    <li class="align"><a href="./restauration.php" class="linkcolor navlink">Restauration</a></li>
+                    <li class="deroulant align"><a href="#" class="linkcolor navlink">Ressources &ensp;</a>
+                        <ul class="sous">
+                            <li><a href="https://bu.univ-gustave-eiffel.fr/collections/ressources-electroniques/de-a-a-z/">Bibliothèque de ressources</a></li>
+                            <li><a href="./pasCree.html">Tutoriels</a></li>
+                            <li><a href="https://etudiant.u-pem.fr/ent-services.php">Mes fichiers</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="align"><a href="./accueil.php"><img src="./img/logoUniversite2.png" alt=""></a></li>
+
+                    <li class="align"><a href="./reservation.php" class="linkcolor navlink">Reservation</a></li>
+                    <li class="deroulant align"><a href="#" class="linkcolor navlink">Cours &ensp;</a>
+                        <ul class="sous">
+                            <li><a href="https://elearning.univ-eiffel.fr/my/">Cours</a></li>
+                            <li><a href="https://edt.univ-eiffel.fr/direct/index.jsp?login=visuedt&password=visuedt">Emploi du temps</a></li>
+                            <li><a href="./note.php">Notes</a></li>
+                            <li><a href="https://elearning.univ-eiffel.fr/calendar/view.php?view=upcoming">Agenda</a></li>
+                        </ul>
+                    </li>
+                    <li class="align"><a href="./compte.php" class="linkcolor navlink">Mon compte</a></li>
+                </ul>
+            </div>
+        </nav>
+
     </header>
-        
+
 
     <main>
         <?php
 
-if (isset($_SESSION["login"])) {
-    echo "<h2>Bonjour <i><strong>{$_SESSION["login"]}</strong></i> bienvenu sur ta page de profil !<BR></h2>";
-}
+        if (isset($_SESSION["login"])) {
+            echo "<h2>Bonjour <i><strong>{$_SESSION["login"]}</strong></i> bienvenu sur ta page de profil !<BR></h2>";
+        }
 
-?>
+        ?>
 
 
 
@@ -132,11 +132,10 @@ if (isset($_SESSION["login"])) {
                     <img src="<?php echo $result["url_pp"]; ?>" alt="image-modifiable" class="profil">
                     <h1 class="nom">
                         <?php
-if (isset($result)) {
-    echo '<h1 class="nom">' . $result['nom'] . ' - ' . $result['prenom'] . '</h1>';
-    
-}
-?> </h1>
+                        if (isset($result)) {
+                            echo '<h1 class="nom">' . $result['nom'] . ' - ' . $result['prenom'] . '</h1>';
+                        }
+                        ?> </h1>
 
                 </div>
                 <div class="bouton1">
@@ -163,31 +162,29 @@ if (isset($result)) {
 
                 </div>
                 <div class="bouton2">
-                    <a href="deconnexion.php" class="bouton"><img src="./img/Deconnexion.svg" alt=""
-                            class="deconnexion">Déconnexion</a>
+                    <a href="deconnexion.php" class="bouton"><img src="./img/Deconnexion.svg" alt="" class="deconnexion">Déconnexion</a>
                 </div>
 
             </div>
             <div class="contenu-info">
                 <?php
-if (isset($result)) {
-    echo '<h1 class="nom2">' . $result['nom'] . ' - ' . $result['prenom'] . '</h1>';
-    
-}
-?>
+                if (isset($result)) {
+                    echo '<h1 class="nom2">' . $result['nom'] . ' - ' . $result['prenom'] . '</h1>';
+                }
+                ?>
 
 
 
 
                 <?php
-if (isset($result)) {
-    echo "<h1 class='info-gene'>Informations générales</h1>";
-    echo "<p>{$result['info_generales']}</p>";
-}
+                if (isset($result)) {
+                    echo "<h1 class='info-gene'>Informations générales</h1>";
+                    echo "<p>{$result['info_generales']}</p>";
+                }
 
 
 
-?>
+                ?>
                 <div class="ajouter">Modifier</div>
 
                 <div class="custom-model-main">
@@ -210,8 +207,7 @@ if (isset($result)) {
                                     <br>
                                     <br>
 
-                                    <textarea id="infos" name="infos" rows="5"
-                                        cols="70">Rentrer les informations...</textarea>
+                                    <textarea id="infos" name="infos" rows="5" cols="70">Rentrer les informations...</textarea>
                                     <br>
                                     <input type="submit" value="Envoyer" class="bouton-envoyer-infos" />
                                 </div>
@@ -234,18 +230,28 @@ if (isset($result)) {
 
     </main>
     <footer>
-       
+        <a href="" class="logoheader"><img src="./img/logoUniversite2.png" alt="" class="logoheader"></a>
+        <div class="footwrapper">
+            <a href="https://www.univ-gustave-eiffel.fr" class="linkfooter" target="_blank">Site de l'université</a>
+            <a href="https://lcs.univ-gustave-eiffel.fr/vie-etudiante/reseaux-detudiants/reseaux-anciens-etudiants" class="linkfooter" target="_blank">Réseau des anciens</a>
+            <a href="https://www.facebook.com/MDEUGE/?locale=fr_FR" class="linkfooter" target="_blank">Maison des étudiants</a>
+        </div>
+        <div class="footwrapper">
+            <a href="mentionLegales.html" class="linkfooter">Mentions légales</a>
+            <a href="plansite.html" class="linkfooter">Plan du site</a>
+            <a href="apropos.html" class="linkfooter">A propos</a>
+        </div>
     </footer>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script>
-    $(function() {
-        $('.ajouter').on('click', function() {
-            $('.custom-model-main').addClass('model-open');
+        $(function() {
+            $('.ajouter').on('click', function() {
+                $('.custom-model-main').addClass('model-open');
+            });
+            $('.close-btn, .bg-overlay').click(function() {
+                $('.custom-model-main').removeClass('model-open');
+            });
         });
-        $('.close-btn, .bg-overlay').click(function() {
-            $('.custom-model-main').removeClass('model-open');
-        });
-    });
     </script>
     <script src="script-burger.js" defer></script>
 </body>
